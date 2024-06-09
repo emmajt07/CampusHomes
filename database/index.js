d16d5dcbb390e4329f78d6fd9a6325a7f397e057
¨emmajt07¨ <emmanuel070804Q@gmail.com>
2024-06-09 13:35:46 -0600

    index

---
+++ database/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import path from 'path';
+import { fileURLToPath } from "url";
+import dotenv from 'dotenv';
+import DB from "./db.js";
+import UserRouter from "../Controllers/UserRouters.js";
+import RestaurantRouter from "../Controllers/RestaurantRouters.js";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = process.env.PORT || 3000;
+
+//configure Handlabers
+app.engine('handlebars', engine ({
+    defaultLayout: 'index',
+}));
+app.set('view engine', 'handlebars');
+
+// Middleware para archivos estaticos en public
+app.use(express.static(__dirname + '/public'));
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", UserRouter);
+app.use("/api/restaurants", RestaurantRouter);
+
+DB.connectDB(process.env.DB_URI);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
